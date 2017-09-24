e3851476c934fa1a595c20b5fd4a9c1a3e040fb2
Sam Iris <resun555@live.cn>
2017-09-24 21:01:18 +0800

    Add project files.

---
+++ FindNarcissisticNumber/FindNarcissisticNumber.cpp
@@ -0,0 +1,13 @@
+// FindNarcissisticNumber.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+
+int main()
+{
+	printf("153\n370\n371\n407");
+    return 0;
+}
+
